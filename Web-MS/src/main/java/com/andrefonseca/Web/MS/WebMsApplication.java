3daeaf02b29ecd74f036b589f3b96d5051778ea1
André Fonseca <decobf15@gmail.com>
2024-04-01 19:49:29 -0300

    first commit

---
+++ Web-MS/src/main/java/com/andrefonseca/Web/MS/WebMsApplication.java
@@ -0,0 +1,13 @@
+package com.andrefonseca.Web.MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMsApplication.class, args);
+	}
+
+}
---
+++ Web-MS/src/test/java/com/andrefonseca/Web/MS/WebMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrefonseca.Web.MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
