afdb706cb1cfe796c7c8e63bc97e32103a5d0fc6
André Fonseca <decobf15@gmail.com>
2024-04-01 20:30:43 -0300

    user entity and resource

---
+++ Web-MS/src/main/java/com/andrefonseca/Web/MS/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.andrefonseca.Web.MS.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andrefonseca.Web.MS.entities.User;
+
+@RestController
+@RequestMapping(value = "/users", method=RequestMethod.GET)
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "954322345", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
