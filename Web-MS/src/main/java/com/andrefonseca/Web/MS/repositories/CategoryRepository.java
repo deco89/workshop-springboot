79fa6b62f550e223ba227b3724fa3fee0efab293
André Fonseca <decobf15@gmail.com>
2024-04-02 16:38:38 -0300

    Category

---
+++ Web-MS/src/main/java/com/andrefonseca/Web/MS/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.andrefonseca.Web.MS.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andrefonseca.Web.MS.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+	
+
+}
